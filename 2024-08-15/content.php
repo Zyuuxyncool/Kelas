483811a0c3ff49aee7dd053f1577a7a73ae9c974
Muhammad Andreas Athallah Saifa Anam <andreasnation970@gmail.com>
2024-11-02 16:21:29 +0700

    2024-08-15

---
+++ 2024-08-15/content.php
@@ -0,0 +1,13 @@
+<?php
+    $sekolah = "Web SMKN 2 Buduran";
+    $judul = "SMKN 2 Buduran";
+    $isi = "ini adalah web entertaint sekolah yang dibuat oleh siswa
+            XI RPL tahun 2024 atas nama Darma tanggal 15 Agustus 1945";
+    $hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat"];
+    $pelajaran = ["matematika", "b.indo", "b.jepang", "bahasa.jawa",
+                  "b.ing", "b.jerman", "PKN", "RPL", "kewiraushaan", "BK",
+                  "pjok", "agama", "sejarah"];
+    $jadwal = "Jadwal Pelajaran RPL tahun 2024/2025";
+    $gambar = ["katana.jpeg", "logoo.png", "m4a1.jpeg"];
+?>
+
